111fd56ef22a5ef8205e43fa92a850ebfd15224a
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2020-09-13 16:59:04 +0300

    Create Line.swift

---
+++ Drawing/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  DrawingTest
+//
+//  Created by JanFranco on 13.09.2020.
+//  Copyright © 2020 janfranco. All rights reserved.
+//
+
+import UIKit
+
+struct Line {
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
